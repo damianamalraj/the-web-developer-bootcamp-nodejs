bd1f8c2b645d1b5bd21a5f419613e85a257f96be
Damian Clitus <daniel.robertamalraj@hotmail.com>
2019-10-31 17:39:48 +0100

    1

---
+++ firstExpressApp/app.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var app = express();
+
+app.get("*", function (req, res) {
+    console.log("Page not found!!!");
+});
+
+app.get("/", function (req, res) {
+    res.send("Hi there!");
+});
+
+app.get("/bye", function (req, res) {
+    res.send("goodbye!!");
+});
+
+app.listen("3000", function () {
+    console.log("server has started");
+});
